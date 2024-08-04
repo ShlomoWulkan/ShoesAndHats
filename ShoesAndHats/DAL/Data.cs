00b930a893170081cfcf12e7df6481db9bde1f1b
Your Name <you@example.com>
2024-08-04 17:37:21 +0300

    Add project files.

---
+++ ShoesAndHats/DAL/Data.cs
@@ -0,0 +1,30 @@
+﻿using MyLibrary.DAL;
+
+namespace ShoesAndHats.DAL
+{
+    public class Data
+    {
+        string connectionString = "server=WULKAN\\SA; initial catalog=ShoesAndHats; user id=SA; password=1234; TrustServerCertificate=Yes";
+
+
+        private static Data _data;
+        private DataLayer DataLayer;
+
+        private Data()
+        {
+            DataLayer = new DataLayer(connectionString);
+        }
+
+        public static DataLayer Get
+        {
+            get
+            {
+                if (_data == null)
+                {
+                    _data = new Data();
+                }
+                return _data.DataLayer;               
+            }
+        }
+    }
+}
---
+++ ShoesAndHats/DAL/DataLayer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoesAndHats.Models;
+
+
+namespace MyLibrary.DAL
+{
+    public class DataLayer : DbContext
+    {
+        public DataLayer(string ConnectionString) : base(GetOptions(ConnectionString))
+        {
+            Database.EnsureCreated();
+            Seed();
+        }
+      
+        private void Seed()
+        {
+          
+        }
+        private static DbContextOptions GetOptions(string ConnectionString)
+        {
+            return new DbContextOptionsBuilder().UseSqlServer(ConnectionString).Options;
+        }
+
+    }
+}
