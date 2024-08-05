b4a925b9a39c23ef728d38daa83207040574ddfd
Your Name <you@example.com>
2024-08-05 11:14:15 +0300

    complit basic view of indexs and the URL image

--- ShoesAndHats/Models/Shoes.cs
+++ ShoesAndHats/Models/Shoes.cs
@@ -7,11 +7,16 @@ namespace ShoesAndHats.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "מידה")]
+        public int Size { get; set; }
+
         [Display(Name ="מותג")]
         public string Brands { get; set; }
 
+        [Display(Name = "צבע")]
         public string Color { get; set; }
 
+        [Display(Name = "קישור לתמונה")]
         public string UrlImage { get; set; }    
     }
 }
