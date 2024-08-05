a8f9d55b1fcb0e5f668adf205df3a236d65feadb
ElkanaRozental <rozentlen@gmail.com>
2024-08-05 10:07:57 +0300

    מחיקה של כובע

---
+++ ShoesAndHats/Controllers/HatsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoesAndHats.DAL;
+using ShoesAndHats.Models;
+
+namespace ShoesAndHats.Controllers
+{
+    public class HatsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Data.Get.Hats);
+        }
+
+        public IActionResult CreateHat()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult CreateHat(Hats? hat)
+        {
+            if (hat == null) return RedirectToAction("CreateHat");
+            Data.Get.Hats.Add(hat);
+            Data.Get.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteHat(int? id)
+        {
+            if (id == null) return RedirectToAction("index");
+            Hats? hat  = Data.Get.Hats.FirstOrDefault(h => h.Id == id);
+            if (hat == null) return RedirectToAction("index");
+            Data.Get.Hats.Remove(hat);
+            Data.Get.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
