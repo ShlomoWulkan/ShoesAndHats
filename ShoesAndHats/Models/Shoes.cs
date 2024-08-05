d0b8428ac4619027c0b7075fc5faa35c7eeaac99
Your Name <you@example.com>
2024-08-05 09:36:09 +0300

    Adding ShoesControler and Basic pages

---
+++ ShoesAndHats/Models/Shoes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoesAndHats.Models
+{
+    public class Shoes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="מותג")]
+        public string Brands { get; set; }
+
+        public string Color { get; set; }
+
+        public string UrlImage { get; set; }    
+    }
+}
